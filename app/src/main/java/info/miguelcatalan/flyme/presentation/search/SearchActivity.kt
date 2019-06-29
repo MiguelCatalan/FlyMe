441a02a36e4243aa0af7e0800b11ece38e43694b
MiguelCatalan <miguel.catalan.banuls@gmail.com>
2019-06-29 09:19:36 +0200

    Migrate to androidx

--- app/src/main/java/info/miguelcatalan/flyme/presentation/search/SearchActivity.kt
+++ app/src/main/java/info/miguelcatalan/flyme/presentation/search/SearchActivity.kt
@@ -1,7 +1,7 @@
 package info.miguelcatalan.flyme.presentation.search
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import info.miguelcatalan.flyme.R
 
 class SearchActivity : AppCompatActivity() {
