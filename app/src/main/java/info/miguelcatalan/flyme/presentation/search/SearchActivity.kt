7911573f5a34d82fd198afafa8030f57c0f5a745
MiguelCatalan <miguel.catalan.banuls@gmail.com>
2019-06-29 08:51:44 +0200

    Create search base android boilerplate

---
+++ app/src/main/java/info/miguelcatalan/flyme/presentation/search/SearchActivity.kt
@@ -0,0 +1,13 @@
+package info.miguelcatalan.flyme.presentation.search
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import info.miguelcatalan.flyme.R
+
+class SearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
