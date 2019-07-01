8689421eabceeba7093c8e5449d598f86f50149a
MiguelCatalan <miguel.catalan.banuls@gmail.com>
2019-07-01 12:30:34 +0200

    Refactor Resource to use the Repository instead of the Api

--- domain/src/main/java/info/miguelcatalan/flyme/domain/airport/AirportResource.kt
+++ domain/src/main/java/info/miguelcatalan/flyme/domain/airport/AirportResource.kt
@@ -1,16 +1,26 @@
 package info.miguelcatalan.flyme.domain.airport
 
+import info.miguelcatalan.flyme.domain.logger.Logger
+import info.miguelcatalan.flyme.domain.repository.RxBaseRepository
 import io.reactivex.Observable
 
 class AirportResource(
-    private val airportApi: AirportApi
+    private val repository: RxBaseRepository<String, Airport>
 ) {
 
     fun searchForAirport(searchTerm: String): Observable<List<Airport>> {
-        return airportApi.getAllAirports()
-            .flatMap { Observable.fromIterable(it) }
+        return repository.getAll()
+            .flatMap {
+                Observable.fromIterable(it)
+            }
             .filter {
-                (it.name.contains(searchTerm) || it.airportCode.contains(searchTerm))
+                (it.name.contains(
+                    searchTerm,
+                    ignoreCase = true
+                ) || it.airportCode.contains(
+                    searchTerm,
+                    ignoreCase = true
+                ))
             }
             .toList()
             .toObservable()
