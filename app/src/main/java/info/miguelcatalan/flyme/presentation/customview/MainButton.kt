a3a31a6ee3f2da48b0537cec7b2d508bd879ddda
MiguelCatalan <miguel.catalan.banuls@gmail.com>
2019-07-02 10:14:47 +0200

    Create custom button view

---
+++ app/src/main/java/info/miguelcatalan/flyme/presentation/customview/MainButton.kt
@@ -0,0 +1,43 @@
+package info.miguelcatalan.flyme.presentation.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import info.miguelcatalan.flyme.R
+import kotlinx.android.synthetic.main.main_button_layout.view.*
+
+class MainButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    var text: Int = 0
+        set(value) {
+            field = value
+            configureLayout()
+        }
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.main_button_layout, this, true)
+    }
+
+    private fun configureLayout() {
+        mainButton.setText(text)
+    }
+
+    fun enable(isEnabled: Boolean) {
+        mainButton.isEnabled = isEnabled
+        when (isEnabled) {
+            true -> mainButton.setTextColor(resources.getColor(R.color.white))
+            false -> mainButton.setTextColor(resources.getColor(R.color.steel))
+        }
+    }
+
+    fun setOnClickListener(onClickPressed: () -> Unit = {}) {
+        mainButton.setOnClickListener {
+            onClickPressed.invoke()
+        }
+    }
+}
+
+
